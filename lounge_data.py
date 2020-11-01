dba69ef7af61b47f956d717bb395709e07644ba3
7r-yi <Afyx-rily@outlook.jp>
2020-11-02 05:14:05 +0900

    Update

--- lounge_data.py
+++ lounge_data.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import sys
 import constant
+
 rm_emoji = dict.fromkeys(range(0x10000, sys.maxunicode + 1), '')
 
 
@@ -14,15 +15,15 @@ def road_spreadsheet(name):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credential = {
         "type": "service_account",
-        "project_id": os.environ['SHEET_PROJECT_ID'],
-        "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
-        "private_key": os.environ['SHEET_PRIVATE_KEY'],
-        "client_email": os.environ['SHEET_CLIENT_EMAIL'],
-        "client_id": os.environ['SHEET_CLIENT_ID'],
-        "auth_uri": os.environ['auth_uri'],
-        "token_uri": os.environ['token_uri'],
-        "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
-        "client_x509_cert_url": os.environ['SHEET_CLIENT_X509_CERT_URL']
+        "project_id": os.environ['PROJECT_ID'],
+        "private_key_id": os.environ['PRIVATE_KEY_ID'],
+        "private_key": os.environ['PRIVATE_KEY'],
+        "client_email": os.environ['CLIENT_EMAIL'],
+        "client_id": os.environ['CLIENT_ID'],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ['CLIENT_X509_CERT_URL']
     }
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
     gc = gspread.authorize(credentials)
